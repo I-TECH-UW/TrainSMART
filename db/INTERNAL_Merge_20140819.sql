34446f41d5e5fa9e086a67fabb33233ca7112f15
tamara7 <tamara7@GHKKZV1.clients.nebula2.washington.edu>
2014-08-21 15:09:50 -0700

    sql database patch file

---
+++ db/INTERNAL_Merge_20140819.sql
@@ -0,0 +1,9 @@
+/*
+2014-07-29 Tamara Astakhova
+For Request: Haiti PreService assignment patch
+*/
+
+INSERT INTO `translation`(`key_phrase`,`phrase`,`modified_by`,`created_by`,`is_deleted`) VALUES ('ps tutor','Teacher',1,null,0);
+INSERT INTO `translation`(`key_phrase`,`phrase`,`modified_by`,`created_by`,`is_deleted`) VALUES ('ps institution','Training Centre',1,null,0);
+INSERT INTO `translation`(`key_phrase`,`phrase`,`modified_by`,`created_by`,`is_deleted`) VALUES ('ps zip code','Postal Code / ZIP',1,null,0);
+
