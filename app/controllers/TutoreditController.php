5cb22ac1523a47736f01d0658b567b287cc77074
tamara7 <tamara7@GHKKZV1.clients.nebula2.washington.edu>
2014-08-22 13:07:02 -0700

    changed allignment to display person info history (top right conner) in edit student/tutor pages

--- app/controllers/TutoreditController.php
+++ app/controllers/TutoreditController.php
@@ -118,6 +118,21 @@ die($query);
 		//$this->view->assign('cell2',$details['person'][0]['phone_mobile_2']);
 		$this->view->assign('cell2',$details['person'][0]['phone_home']);
 		$this->view->assign('dob',$dob);
+		
+		//TA:6: added 8/8/2014 - 8/10/2014
+		$dc = strtotime($details['person'][0]['timestamp_created']);
+		$dateCreated = $dc != '' && $dc > 0 ? date("d-m-Y",$dc) : "N/A";
+		$this->view->assign('dateCreated',$dateCreated);
+		$dm = strtotime($details['person'][0]['timestamp_updated']);
+		$dateModified = $dm != '' && $dm >0 ? date("d-m-Y",$dm): "N/A";
+		$this->view->assign('dateModified',$dateModified);
+		$this->view->assign('uuid',$details['person'][0]['uuid']);
+		require_once ('models/table/Person.php');
+		$personObj = new Person ( );
+		$created_by = $details['person'][0]['created_by'] ? $personObj->getPersonName($details['person'][0]['created_by']) : "N/A";
+		$this->viewAssignEscaped('creator', $created_by);
+		$update_by = $details['person'][0]['modified_by'] ? $personObj->getPersonName($details['person'][0]['modified_by']) : "N/A";
+		$this->viewAssignEscaped('updater', $update_by);
 
 		$helper = new Helper();
 
@@ -235,6 +250,7 @@ die($query);
 		$this->view->assign('yearofstudy',($details['student'][0]['yearofstudy'] != 0 ? $details['student'][0]['yearofstudy'] : ""));
 
 */
+		
 		// For Title List
 		$listtitle = $Tutoredit->ListTitle();
 		$this->view->assign('gettitle',$listtitle);
