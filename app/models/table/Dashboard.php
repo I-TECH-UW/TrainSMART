26548aa41cbe53a07eaf4d453c61c6b26e630d99
Greg Rossum <rossumg@uw.edu>
2014-10-01 16:08:23 -0700

    Commit Greg's employee and dashboard changes

---
+++ app/models/table/Dashboard.php
@@ -0,0 +1,94 @@
+<?php
+require_once('ITechTable.php');
+require_once('Helper.php');
+
+class Dashboard extends ITechTable
+{
+	protected $_primary = 'id';
+	protected $_name = 'institution';
+
+	public function fetchdetails() {
+		$output = array();
+
+		$helper = new Helper();
+		$db = Zend_Db_Table_Abstract::getDefaultAdapter ();
+
+		// GETTING CURRENT USER ID
+		$uid = $helper->myid();
+
+		// GETTING INSTITUTION IDS
+		$ids = $helper->getUserInstitutions($uid,false);
+		if (count ($ids) > 0){
+			// LIMITING INSTITUTIONS TO THE ONES ENABLED FOR THIS USER
+			$select = $db->select()
+				->from($this->_name)
+				->where("id IN (" . implode(",", $ids) . ")");
+		} else {
+			// SHOWING ALL INSTITUTIONS
+			$select = $db->select()
+				->from($this->_name);
+		}
+
+		$result = $db->fetchAll($select);
+		foreach ($result as $row){
+			$students	= $helper->getInstitutionStudents($row['id'],"all","count");
+			$grads		= $helper->getInstitutionStudents($row['id'],"graduated","count");
+			$dropped	= $helper->getInstitutionStudents($row['id'],"dropped","count");
+			$tutors		= $helper->getInstitutionTutorCount($row['id']);
+
+			$output[] = array(
+				"col1" => $row['institutionname'],
+				"col2" => $students,
+				"col3" => $tutors,
+				"col4" => ((is_numeric($tutors)) && ($tutors > 0) && (is_numeric($students)) && ($students > 0)) ? "1 : " . round(($students / $tutors),2) : "N/A",
+				"col5" => $grads,
+				"col6" => 0,
+				"link" => Settings::$COUNTRY_BASE_URL . "/institution/institutionedit/id/" . $row['id'],
+				"type" => 1
+			);
+			$cohorts = $this->fetchCohorts($row['id']);
+			foreach ($cohorts as $cohort){
+				$output[] = array(
+					"col1" => $cohort['name'],
+					"col2" => $cohort['count'],
+					"col3" => "",
+					"col4" => "",
+					"col5" => "",
+					"col6" => "",
+					"link" => Settings::$COUNTRY_BASE_URL . "/cohort/cohortedit/id/" . $cohort['id'],
+					"type" => 2
+				);
+			}
+		}
+		return $output;
+	}
+
+	public function fetchCohorts($iid){
+		$output = array();
+
+		$db = Zend_Db_Table_Abstract::getDefaultAdapter ();
+		$select = $db->select()
+			->from("cohort")
+			->where("institutionid = ?",$iid);
+		$result = $db->fetchAll($select);
+		foreach ($result as $row){
+			$output[] = array(
+				"id" => $row['id'],
+				"name" => trim($row['cohortid'] . " " . $row['cohortname']),
+				"count" => $this->fetchCohortStudents($row['id'])
+			);
+		}
+		return $output;
+	}
+
+	public function fetchCohortStudents($cid){
+		$output = array();
+		$db = Zend_Db_Table_Abstract::getDefaultAdapter ();
+		$select = $db->select()
+			->from("link_student_cohort")
+			->where("id_cohort = ?",$cid);
+		$result = $db->fetchAll($select);
+		return count($result);
+	}
+}
+?>
---
+++ sites/globals.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Created on Feb 11, 2008
+ *
+*  Built for I-Tech
+*  Fuse IQ -- todd@fuseiq.com
+*
+*/
+
+ini_set('max_execution_time','300');
+ini_set('memory_limit', '1024M');
+
+define('space',  " ");
+
+
+class Globals {
+	public static $BASE_PATH = '/wamp/www/trainsmart.git/';
+	public static $WEB_FOLDER = 'html';
+	//orig 	
+	public static $COUNTRY = 'test';
+	//public static $COUNTRY = 'abri';
+
+	public function __construct() {
+
+		require_once('settings.php');
+/* orig
+		error_reporting( E_ALL ^ E_NOTICE); // E_ALL | E_NOTICE// & E_STRICT); // todo ... set back to normal
+		// | E_STRICT
+		//);
+		$parts = array('jhpiego','ethiopia','trainingdata','org');
+		$countryLoaded = false;
+		if ( $_SERVER['HTTP_HOST'] == 'jhpiego.ethiopia.trainingdata.org')
+			Settings::$DB_DATABASE = 'itechweb_jpeigo_ethiopa';
+			*/
+
+		// PATH_SEPARATOR =  ; for windows, : for *nix
+		$iReturn = ini_set( 'include_path',
+					(Globals::$BASE_PATH).PATH_SEPARATOR.
+					(Globals::$BASE_PATH).'app'.PATH_SEPARATOR.
+					(Globals::$BASE_PATH.'ZendFramework'.DIRECTORY_SEPARATOR.'library').PATH_SEPARATOR.
+						ini_get('include_path'));
+		require_once 'Zend/Loader.php';
+
+		require_once 'Zend/Db.php';
+		//fixes mysterious configuration issue
+		require_once('Zend/Db/Adapter/Pdo/Mysql.php');
+		//set a default database adaptor		
+		$db = Zend_Db::factory('PDO_MYSQL', array(
+			'host'     => Settings::$DB_SERVER,
+			'username' => Settings::$DB_USERNAME,
+			'password' => Settings::$DB_PWD,
+			'dbname'   => Settings::$DB_DATABASE
+		));
+		 require_once 'Zend/Db/Table/Abstract.php';
+		Zend_Db_Table_Abstract::setDefaultAdapter($db);
+		}
+}
+
+new Globals();
+
