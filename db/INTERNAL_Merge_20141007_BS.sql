a1702e8c0eb6f20300c7faf7659b264bfc529117
Benjamin Smith <bens23@washington.edu>
2014-10-09 11:19:49 -0700

    fix for broken insert.

--- db/INTERNAL_Merge_20141007_BS.sql
+++ db/INTERNAL_Merge_20141007_BS.sql
@@ -4,5 +4,6 @@ Ben Smith
 For Request: PEPFAR South Africa Employee Module Translation
 */
 
-INSERT INTO `translation` (`key_phrase`, `phrase`, `modified_by`, `is_deleted`) VALUES ('Employee', 'Employee', '1');
-INSERT INTO `translation` (`key_phrase`, `phrase`, `modified_by`, `is_deleted`) VALUES ('Employees', 'Employees', '1', );
+INSERT INTO `translation` (`key_phrase`, `phrase`, `modified_by`, `is_deleted`) VALUES ('Employee', 'Employee', '1', '0');
+INSERT INTO `translation` (`key_phrase`, `phrase`, `modified_by`, `is_deleted`) VALUES ('Employees', 'Employees', '1', '0');
+
