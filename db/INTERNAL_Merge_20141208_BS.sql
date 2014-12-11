c7bcff9996b09d6f30c9695f423ac623af163152
Benjamin Smith <benjamin.coder.smith@gmail.com>
2014-12-10 23:19:18 -0800

    add more columns to mechanism table

--- db/INTERNAL_Merge_20141208_BS.sql
+++ db/INTERNAL_Merge_20141208_BS.sql
@@ -1,2 +1,3 @@
-ALTER TABLE `mechanism_option`
-ADD COLUMN `owner_id`  int(11) NOT NULL DEFAULT 0;
+ALTER TABLE `mechanism_option` ADD COLUMN `owner_id` int(11) NOT NULL DEFAULT 0 AFTER `mechanism_phrase`;
+ALTER TABLE `mechanism_option` ADD COLUMN `funder_id` int(11) NOT NULL DEFAULT 0 AFTER `owner_id`;
+ALTER TABLE `mechanism_option` ADD COLUMN `external_id` int(11) NOT NULL DEFAULT 0 AFTER `funder_id`;
