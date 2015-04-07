1c41bc4c44ebee613de940091d54d7494ebe1231
Benjamin Smith <bens23@washington.edu>
2015-04-07 13:58:18 -0700

    fix for bad sql

--- db/INTERNAL_Merge_20150402_BS_class_modules.sql
+++ db/INTERNAL_Merge_20150402_BS_class_modules.sql
@@ -1,10 +1,8 @@
 ALTER TABLE `classes`
 ADD COLUMN `credits`  int(10) NOT NULL DEFAULT 0 AFTER `coursetopic`,
-ADD COLUMN `class_modules_id`  int(10) NOT NULL DEFAULT 0 AFTER `credits`;
+ADD COLUMN `class_modules_id`  int(10) NOT NULL DEFAULT 0 AFTER `credits`,
 ADD COLUMN `custom_1`  varchar(255) NULL AFTER `class_modules_id`,
 ADD COLUMN `custom_2`  varchar(255) NULL AFTER `custom_1`;
-
-ADD COLUMN `coursetypeid`  int(10) NULL DEFAULT 0 AFTER `title`;
 
 CREATE TABLE `class_modules` (
   `id` int(10) NOT NULL auto_increment,
