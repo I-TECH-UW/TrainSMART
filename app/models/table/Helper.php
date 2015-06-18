d6099c88972c0ec1bab57ff885294d5a38ca60e3
Tamara Astakhova <tamara7@uw.edu>
2015-06-18 12:01:36 -0700

    Pre-service admin able to delete a degree.

--- app/models/table/Helper.php
+++ app/models/table/Helper.php
@@ -1136,10 +1136,13 @@ class Helper extends ITechTable
 		return $result;
 	}
 
+	//TA:35 
 	public function AdminDegrees(){
-		$select = $this->dbfunc()->select()
-			->from("lookup_degrees")
-			->order('degree');
+// 		$select = $this->dbfunc()->select()
+// 			->from("lookup_degrees")
+// 			->order('degree');
+		$select = "select lookup_degrees.id, lookup_degrees.degree,  link_institution_degrees.id as used from lookup_degrees
+		left join link_institution_degrees on link_institution_degrees.id_degree = lookup_degrees.id group by lookup_degrees.id";
 		$result = $this->dbfunc()->fetchAll($select);
 		return $result;
 	}
