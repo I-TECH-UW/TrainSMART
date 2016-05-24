fa270e8f8bf396133c733a9fbdbe10980d14a191
Ben Smith <bens23@uw.edu>
2016-05-24 11:29:44 -0700

    make classid a foreign key in link_student_classes to denote errors in class assignments

--- db/INTERNAL_Merge_20160517_BS_certificate_issuer_fix.sql
+++ db/INTERNAL_Merge_20160517_BS_certificate_issuer_fix.sql
@@ -1,2 +1,3 @@
 ALTER TABLE certificate_issuers MODIFY COLUMN id INT auto_increment;
 ALTER TABLE certificate_issuers CHANGE COLUMN issuer_logo_url issuer_logo_file_id INT;
+ALTER TABLE link_student_classes ADD CONSTRAINT fk_classid FOREIGN KEY (`classid`) REFERENCES classes(`id`);
