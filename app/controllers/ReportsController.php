4a5f160d4897559909247d3c8ed2c35a586bbef5
Tamara Astakhova <tamara7@uw.edu>
2016-08-05 12:48:12 -0700

    Fix bug with cohort institution filtering.

--- app/controllers/ReportsController.php
+++ app/controllers/ReportsController.php
@@ -6528,9 +6528,12 @@ join user_to_organizer_access on user_to_organizer_access.training_organizer_opt
 			if (isset($params['cohort']) && $params['cohort']) {
 				$s->where('c.id = ?', $params['cohort']);
 			}
-			//TA filter cohort by institution access as well
-			$uid = $helper->myid();
-			$s->where("c.institutionid IN (SELECT institutionid FROM link_user_institution WHERE userid = ?)", $uid);
+			//TA  filter cohort by institution access as well
+  			$uid = $helper->myid();
+ 			$user_institutions = $helper->getUserInstitutions($uid);
+ 		    if (!empty($user_institutions)) {
+ 			   $s->where("c.institutionid IN (SELECT institutionid FROM link_user_institution WHERE userid = ?)", $uid);
+ 		    }
 		}
 
 		if (isset($params['cadre']) && $params['cadre'] ||
