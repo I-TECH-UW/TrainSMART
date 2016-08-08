ba861508f2abb7f5f049ff37cbae8ed302331bda
Tamara Astakhova <tamara7@uw.edu>
2016-08-08 14:26:02 -0700

    Fix geo filtering reports by participants name.

--- app/views/helpers/EditTableHelper.php
+++ app/views/helpers/EditTableHelper.php
@@ -112,7 +112,6 @@ class EditTableHelper {
 			 $colDefsClone[$key] = '{key:"' . htmlspecialchars($key) . 
 			 '", width:'.($key == 'first_name'?80:$key == 'last_name'?80:$key == 'facility_name'?80:$key == 'training_title'?120:(strlen($lbl)*6)).', resizeable:true , label:"' . htmlspecialchars($lbl) . '"' . (!in_array($key, $colStatic) ? ', editor:"textbox"' : '') . $customDef . '}';
             }
-            print $colDefsClone[$key] . "***";
 		}
 
 		// Format data
