b5bdbd9f1db69118ad8fefe9b2e213352d50368c
Ben Smith <bens23@uw.edu>
2016-08-08 14:32:04 -0700

    Rename foreign key acl sql file to be consistently cased with other sql patches. Add default country settings file so that not every module is enabled on new site instances.

---
+++ db/INTERNAL_Merge_20160808_BS_default_country_settings.sql
@@ -0,0 +1,12 @@
+UPDATE _system
+SET module_evaluation_enabled = 0,
+ module_approvals_enabled = 0,
+ module_historical_data_enabled = 1,
+ module_unknown_participants_enabled = 0,
+ module_attendance_enabled = 0,
+ display_training_partner = 0,
+ display_mod_skillsmart = 0,
+ module_employee_enabled = 0,
+ display_country_reports = 1,
+ display_use_offline_app = 0,
+ locale = "en_EN.UTF-8";
