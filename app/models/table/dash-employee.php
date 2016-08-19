39616fdeb7f09ffe3ef1cc27638c9b025d35b4d2
Ben Smith <bens23@uw.edu>
2016-08-19 11:19:56 -0700

    remove commented-out code and warning cleanups

--- app/models/table/dash-employee.php
+++ app/models/table/dash-employee.php
@@ -4,59 +4,47 @@ require_once('Helper.php');
 
 class DashviewEmployee extends Dashview
 {
-	protected $_primary = 'id';
-	protected $_name = 'partner';
+    protected $_primary = 'id';
+    protected $_name = 'partner';
 
-	public function fetchdetails($where = null) {
-		$output = array();
+    public function fetchdetails($where = null)
+    {
+        $output = array();
 
-		$helper = new Helper();
+        $db = Zend_Db_Table_Abstract::getDefaultAdapter();
+        $select = $db->select()
+            ->from($this->_name);
+        if ($where) // comma seperated string for sql
+            $select = $select->where($where);
 
-		$db = Zend_Db_Table_Abstract::getDefaultAdapter ();
-		$select = $db->select()
-			->from($this->_name);
-		if ($where) // comma seperated string for sql
-			$select = $select->where($where);
+        $result = $db->fetchAll($select);
 
-		$result = $db->fetchAll($select);
+        $employees = $this->fetchEmployeeCounts();
 
-		$employees  = $this->fetchEmployeeCounts();
+        foreach ($result as $row) {
+            $output[] = array(
+                "col1" => $row['partner'],
+                "col2" => isset($employees[$row['id']]) ? $employees[$row['id']] : 0,
+                "link" => Settings::$COUNTRY_BASE_URL . "/partner/edit/id/" . $row['id'],
+                "type" => 1
+            );
+        }
+        return $output;
+    }
 
-		foreach ($result as $row){
-			$output[] = array(
-				"col1" => $row['partner'],
-				"col2" => $employees[$row['id']] ? $employees[$row['id']] : 0,
-				"link" => Settings::$COUNTRY_BASE_URL . "/partner/edit/id/" . $row['id'],
-				"type" => 1
-			);
-			// foreach ($cohorts as $cohort){
-			// 	$output[] = array(
-			// 		"col1" => $cohort['name'],
-			// 		"col2" => $cohort['count'],
-			// 		"link" => Settings::$COUNTRY_BASE_URL . "/employee/edit/id/" . $employee['id'],
-			// 		"type" => 2
-			// 	);
-			// }
-		}
-		return $output;
-	}
+    public function fetchEmployeeCounts()
+    {
+        $db = $this->dbfunc();
 
-	public function fetchEmployeeCounts()
-	{
-		$db = $this->dbfunc();
-		//$select = $db->select()->distinct()->from('employee',array('partner_id,count(id) as cnt'));
-		//$select = $select->group('partner_id');
-		
-		$sql_string = 'SELECT DISTINCT `employee`.`partner_id`, count(id) AS `cnt` FROM `employee` GROUP BY `partner_id`';
-		
-		$result = $db->fetchAll($sql_string);
-		$ret = array();
-		if (! count($result))
-			return array();
-		foreach ($result as $key => $row)
-			$ret[$row['partner_id']] = $row['cnt'];
+        $sql_string = 'SELECT DISTINCT `employee`.`partner_id`, count(id) AS `cnt` FROM `employee` GROUP BY `partner_id`';
 
-		return ($ret ? $ret : array());
-	}
+        $result = $db->fetchAll($sql_string);
+        $ret = array();
+        if (!count($result))
+            return array();
+        foreach ($result as $key => $row)
+            $ret[$row['partner_id']] = $row['cnt'];
+
+        return ($ret ? $ret : array());
+    }
 }
-?>
