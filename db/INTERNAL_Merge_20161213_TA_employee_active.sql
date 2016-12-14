28cfbd5ee41263e629542e5512e37ffa4e2ae424
Tamara Astakhova <tamara7@uw.edu>
2016-12-14 13:50:45 -0800

    Employee edit page: total cost format changes.

--- db/INTERNAL_Merge_20161213_TA_employee_active.sql
+++ db/INTERNAL_Merge_20161213_TA_employee_active.sql
@@ -1,7 +1,12 @@
 /*
 2016-12-13 
 Tamara Astakhova
-Add active field to employee add/edit page
+Add active field to employee add/edit page. Change cost fields to two places decimal type
 */
 
 ALTER TABLE employee ADD is_active tinyint(1) NOT NULL DEFAULT '1';
+ALTER TABLE employee CHANGE COLUMN salary salary DECIMAL(10,2) DEFAULT '0.00' ;
+ALTER TABLE employee CHANGE COLUMN benefits benefits DECIMAL(10,2) DEFAULT '0.00' ;
+ALTER TABLE employee CHANGE COLUMN additional_expenses additional_expenses DECIMAL(10,2) DEFAULT '0.00' ;
+ALTER TABLE employee CHANGE COLUMN stipend stipend DECIMAL(10,2) DEFAULT '0.00' ;
+ALTER TABLE employee CHANGE COLUMN annual_cost annual_cost DECIMAL(10,2) DEFAULT '0.00' ;
