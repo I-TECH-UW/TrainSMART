17869ca735e914b740864cc18fd7f8d6ec357c83
Tamara Astakhova <tamara7@uw.edu>
2016-12-19 17:37:23 -0800

    Training roster persons column test.

--- app/controllers/TrainingController.php
+++ app/controllers/TrainingController.php
@@ -2392,7 +2392,6 @@ class TrainingController extends ReportFilterHelpers {
 
 		/* Participants */
 		$persons = PersonToTraining::getParticipants ( $training_id )->toArray ();
-		print_r($persons);
 		//TA:#317
 		$personsFields = array ('first_name' => t ( 'First name' ), 'middle_name' => t ( 'Middle name' ), 'last_name' => t ( 'Last name' ));
 		
