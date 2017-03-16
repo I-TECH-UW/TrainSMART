8e3b878af20ef8ba2f5a6a99abeab55285b47b4d
Tamara Astakhova <tamara7@uw.edu>
2017-03-16 10:58:01 -0700

    Changes in comments #376 to #374.

--- db/INTERNAL_Merge_20170316_TA_employee_contract_start_date.sql
+++ db/INTERNAL_Merge_20170316_TA_employee_contract_start_date.sql
@@ -2,7 +2,7 @@
 2017-03-16
 Tamara Astakhova
 Employee edit contract start date
-#376
+#374
 **********************************************************************/
 
 ALTER TABLE `employee` ADD COLUMN `agreement_start_date` DATE NULL DEFAULT NULL AFTER `external_funding_percent`;
