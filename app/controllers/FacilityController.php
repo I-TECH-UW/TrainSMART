33d1ef31b18d2a2de482030b52249ae102102eba
Tamara Astakhova <tamara7@uw.edu>
2017-03-31 17:37:21 -0700

    Ukraine facility type is not required.

--- app/controllers/FacilityController.php
+++ app/controllers/FacilityController.php
@@ -88,7 +88,6 @@ class FacilityController extends ReportFilterHelpers {
 		$this->viewAssignEscaped ( 'facility_types', OptionList::suggestionList ( 'facility_type_option', 'facility_type_phrase', false, false ) );
 		$this->viewAssignEscaped ( 'facility_sponsors', OptionList::suggestionList ( 'facility_sponsor_option', 'facility_sponsor_phrase', false, false ) );
 		
-		print "=" . $_SERVER ['SERVER_NAME'] . "=";
 	}
 	
 	protected function validateAndSave($facilityRow, $checkName = true) {
@@ -104,7 +103,9 @@ class FacilityController extends ReportFilterHelpers {
 		}
 		
 		// validate fields
-		$status->checkRequired ( $this, 'facility_type_id', t ( 'Facility type' ) ); //TA:#382
+		if($_SERVER ['SERVER_NAME'] !== 'phc.trainingdata.org'){ //TA: do not make facility type required for Ukraine
+		  $status->checkRequired ( $this, 'facility_type_id', t ( 'Facility type' ) ); //TA:#382
+		}
 		$status->checkRequired ( $this, 'facility_province_id', $this->tr ( 'Region A (Province)' ) );
 		if ($this->setting ( 'display_region_b' ))
 			$status->checkRequired ( $this, 'facility_district_id', $this->tr ( 'Region B (Health District)' ) );
