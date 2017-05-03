78f218819ad2d26b2306445e6b4a02084c3392cc
Tamara Astakhova <tamara7@uw.edu>
2017-05-02 22:34:52 -0700

    Fix facility filtering in person add page.

---
+++ db/INTERNAL_Merge_20170502_TA_student_grade_desciption_#402.sql
@@ -0,0 +1,8 @@
+/**********************************************************************
+2017-05-02
+Tamara Astakhova
+Student grade desciption
+#402
+**********************************************************************/
+
+ALTER TABLE link_student_classes ADD COLUMN `grade_description` VARCHAR(45) NULL AFTER `camark`;
