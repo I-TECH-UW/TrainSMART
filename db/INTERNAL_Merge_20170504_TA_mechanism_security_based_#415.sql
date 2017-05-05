7029e7b9e4efa847de55591c68e99cc4cf2d278c
Tamara Astakhova <tamara7@uw.edu>
2017-05-05 14:55:13 -0700

    New User (Activity Managers): user edit/add mechanism access (#415).

---
+++ db/INTERNAL_Merge_20170504_TA_mechanism_security_based_#415.sql
@@ -0,0 +1,18 @@
+/**********************************************************************
+2017-05-4
+Tamara Astakhova
+New User (Activity Managers) - mechanism security based
+#415
+**********************************************************************/
+
+CREATE TABLE `user_to_mechanism_access` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mechanism_option_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `timestamp_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `created_by` (`created_by`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
