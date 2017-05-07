73fb8e0b0f8939a2c2a575800d6d209e3a94ddd1
Tamara Astakhova <tamara7@uw.edu>
2017-05-07 08:11:52 -0700

    New SDS fields: #416.

---
+++ db/INTERNAL_Merge_20170506_TA_facility_sds_fields#416.sql
@@ -0,0 +1,20 @@
+/**********************************************************************
+2017-05-06
+Tamara Astakhova
+Facility new fields: Direct Service Delivery Model and Direct Service Delivery Team.
+#416
+**********************************************************************/
+CREATE TABLE `facility_dsdmodel_option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) DEFAULT NULL,
+  `facility_dsdmodel_phrase` varchar(128) NOT NULL DEFAULT '',
+  `modified_by` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `timestamp_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `timestamp_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_unique` (`facility_dsdmodel_phrase`),
+  KEY `modified_by` (`modified_by`),
+  KEY `created_by` (`created_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
