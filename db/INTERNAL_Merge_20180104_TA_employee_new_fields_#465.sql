a0a2bef057494972d14d2f3faebee7a17769d98f
Tamara Astakhova <tamara7@uw.edu>
2018-01-05 12:33:12 -0800

    Add db changes file (#465).

---
+++ db/INTERNAL_Merge_20180104_TA_employee_new_fields_#465.sql
@@ -0,0 +1,18 @@
+/**********************************************************************
+2017-01-04
+Tamara Astakhova
+Employee new fields
+#465 - #474
+**********************************************************************/
+
+ALTER TABLE employee ADD COLUMN salary_or_stipend VARCHAR(45) NOT NULL DEFAULT '';
+
+
+
+
+
+
+
+salary_or_stipend
+t('Salaried or Stipend')
+
