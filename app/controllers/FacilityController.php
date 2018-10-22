0f07ecd06ee2fa60e860204b3ccd5aecc661b94b
tamar <tamar@10.0.0.8>
2018-10-21 18:24:49 -0700

    New facility name changes (#525).

--- app/controllers/FacilityController.php
+++ app/controllers/FacilityController.php
@@ -201,7 +201,7 @@ class FacilityController extends ReportFilterHelpers {
 				// map db field names to FORM field names
 				//TA:#525
 			    if($this->getSanParam ( 'action' ) === 'add'){
-			        $facilityRow->facility_name = str_replace('"','\'', $this->getSanParam ( 'facility_name' ));
+			        $facilityRow->facility_name = str_replace('"','\'\'', $this->getSanParam ( 'facility_name' ));
 			    }else{
 			        $facilityRow->facility_name = $this->getSanParam ( 'facility_name' );
 			    }
